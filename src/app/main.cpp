7c6774849b2e9cd2b5ea5f76e19fb0d65c86259c
Tsingbing <767575850@qq.com>
2023-04-05 23:10:24 +0800

    feat：添加配置文件目录

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,12 +1,12 @@
 ﻿
-#include <3rdparty/toml11/toml.hpp>
+//#include <3rdparty/toml11/toml.hpp>
 #include <filesystem>
 #include <iostream>
 
 using namespace std;
 
 int main() {
-  auto config = toml::parse("config.toml");
+  //auto config = toml::parse("config.toml");
   cout << "Hello World!" << endl;
   return 0;
 }
--- src/app2/main.cpp
+++ src/app2/main.cpp
@@ -1,11 +1,20 @@
-﻿//#include "toml11/toml.hpp"
+﻿#include "toml11/toml.hpp"
 
 #include <filesystem>
 #include <iostream>
 
 using namespace std;
 
-int main() {
-  cout << "Hello World!" << endl;
+int main() 
+{
+  std::ifstream file("config.toml");
+  std::string content((std::istreambuf_iterator<char>(file)),
+                      (std::istreambuf_iterator<char>()));
+  std::cout << content << std::endl;
+
+  // auto toml = toml::parse("config.toml");
+  // std::cout << "Application directory path: " << std::filesystem::current_path() << '\n';
+  
+  // cout << "Hello World!" << endl;
   return 0;
 }
