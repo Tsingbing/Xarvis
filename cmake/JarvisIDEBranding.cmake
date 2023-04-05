275c89fbc458f590c59d67940b9372ed8b6a33a8
Tsingbing <767575850@qq.com>
2023-04-05 21:32:01 +0800

    init commit

---
+++ cmake/JarvisIDEBranding.cmake
@@ -0,0 +1,14 @@
+﻿#BINARY_ARTIFACTS_BRANCH = master
+#PROJECT_USER_FILE_EXTENSION = .user
+
+set(IDE_VERSION "1.0.0")                            # The IDE version.
+set(IDE_VERSION_COMPAT "1.0.0")                     # The IDE Compatibility version.
+set(IDE_VERSION_DISPLAY "1.0.0")                     # The IDE display version.
+set(IDE_COPYRIGHT_YEAR "2022")                        # The IDE current copyright year.
+
+set(IDE_SETTINGSVARIANT "Jarvis")                  # The IDE settings variation.
+set(IDE_COPY_SETTINGSVARIANT "Tage")                 # The IDE settings to initially import.
+set(IDE_DISPLAY_NAME "Jarvis")                        # The IDE display name.
+set(IDE_ID "jarvis")                               # The IDE id (no spaces, lowercase!)
+set(IDE_CASED_ID "Jarvis")                         # The cased IDE id (no spaces!)
+set(IDE_BUNDLE_IDENTIFIER "org.qt-project.${IDE_ID}") # The macOS application bundle identifier.
---
+++ src/app2/main.cpp
@@ -0,0 +1,11 @@
+﻿//#include "toml11/toml.hpp"
+
+#include <filesystem>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "Hello World!" << endl;
+  return 0;
+}
